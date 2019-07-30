553254de49bc58f8b4f1d141e3390b477715e227
nodejs-machine <kekarjawalekarsachin1@gmail.com>
2019-07-30 14:51:22 +0530

    added margin to the left and right of the card

--- frontend/website/pages/home.php
+++ frontend/website/pages/home.php
@@ -1,94 +1,143 @@
 <section id="about" class="bg-primary text-white mb-0">
-    <div class="container">
-        <h2 class="text-uppercase text-center text-white">About</h2>
-        <hr class="star-light mb-5" />
-        <div class="row">
-            <div class="col-lg-4 ml-auto">
-                <p class="lead">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum
-                    at varius vel pharetra vel turpis nunc eget lorem. Quis viverra nibh
-                    cras pulvinar mattis nunc sed. Aliquet eget sit amet tellus cras
-                    adipiscing enim eu. Etiam erat velit scelerisque in dictum non
-                    consectetur a erat. Et egestas quis ipsum suspendisse ultrices
-                    gravida. Sapien pellentesque habitant morbi tristique senectus et
-                    netus et malesuada. Ornare lectus sit amet est placerat. Viverra
-                    aliquet eget sit amet tellus cras. Amet est placerat in egestas erat
-                    imperdiet sed euismod.
-                </p>
-            </div>
-            <div class="col-lg-4 mr-auto">
-                <p class="lead">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum
-                    at varius vel pharetra vel turpis nunc eget lorem. Quis viverra nibh
-                    cras pulvinar mattis nunc sed. Aliquet eget sit amet tellus cras
-                    adipiscing enim eu. Etiam erat velit scelerisque in dictum non
-                    consectetur a erat. Et egestas quis ipsum suspendisse ultrices
-                    gravida. Sapien pellentesque habitant morbi tristique senectus et
-                    netus et malesuada. Ornare lectus sit amet est placerat. Viverra
-                    aliquet eget sit amet tellus cras. Amet est placerat in egestas erat
-                    imperdiet sed euismod.
-                </p>
-            </div>
-        </div>
-        <div class="text-center mt-4">
-            <a class="btn btn-outline-light btn-xl" role="button" href="/about/">Know More&nbsp;<span></span><i
-                    class="fa fa-chevron-right mr-2"></i></a>
-        </div>
+  <div class="container">
+    <h2 class="text-uppercase text-center text-white">About</h2>
+    <hr class="star-light mb-5" />
+    <div class="row">
+      <div class="col-lg-4 ml-auto">
+        <p class="lead">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum
+          at varius vel pharetra vel turpis nunc eget lorem. Quis viverra nibh
+          cras pulvinar mattis nunc sed. Aliquet eget sit amet tellus cras
+          adipiscing enim eu. Etiam erat velit scelerisque in dictum non
+          consectetur a erat. Et egestas quis ipsum suspendisse ultrices
+          gravida. Sapien pellentesque habitant morbi tristique senectus et
+          netus et malesuada. Ornare lectus sit amet est placerat. Viverra
+          aliquet eget sit amet tellus cras. Amet est placerat in egestas erat
+          imperdiet sed euismod.
+        </p>
+      </div>
+      <div class="col-lg-4 mr-auto">
+        <p class="lead">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum
+          at varius vel pharetra vel turpis nunc eget lorem. Quis viverra nibh
+          cras pulvinar mattis nunc sed. Aliquet eget sit amet tellus cras
+          adipiscing enim eu. Etiam erat velit scelerisque in dictum non
+          consectetur a erat. Et egestas quis ipsum suspendisse ultrices
+          gravida. Sapien pellentesque habitant morbi tristique senectus et
+          netus et malesuada. Ornare lectus sit amet est placerat. Viverra
+          aliquet eget sit amet tellus cras. Amet est placerat in egestas erat
+          imperdiet sed euismod.
+        </p>
+      </div>
     </div>
+    <div class="text-center mt-4">
+      <a class="btn btn-outline-light btn-xl" role="button" href="/about/"
+        >Know More&nbsp;<span></span><i class="fa fa-chevron-right mr-2"></i
+      ></a>
+    </div>
+  </div>
 </section>
 <div id="home" style="background: lightgray">
-    <div class="container" style="padding-top:20px; padding-bottom: 20px;">
-        <div class="card-group row">
-            <div class="card col-lg-4 col-md-4 col-sm-12">
-                <div class="card-body">
-                    <h4 class="card-title text-center">Announcements</h4>
-                    <hr />
-                    <ul class="list-group">
-                        <li class="list-group-item list-group-item-action">Announcement 1</li>
-                        <li class="list-group-item list-group-item-action">Announcement 2</li>
-                        <li class="list-group-item list-group-item-action">Announcement 3</li>
-                    </ul>
-                </div>
+  <div class="container" style="padding-top:20px; padding-bottom: 20px;">
+    <div class="card-group row">
+      <div class="col-lg-4 col-md-4 col-sm-12">
+        <div class="card ml-1 mr-1">
+          <div class="card-body">
+            <h4 class="card-title text-center">Announcements</h4>
+            <hr />
+            <ul class="list-group">
+              <li class="list-group-item list-group-item-action">
+                Announcement 1
+              </li>
+              <li class="list-group-item list-group-item-action">
+                Announcement 2
+              </li>
+              <li class="list-group-item list-group-item-action">
+                Announcement 3
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-4 col-sm-12">
+        <div class="card ml-1 mr-1">
+          <div class="card-body">
+            <h4 class="card-title text-center">Institutes</h4>
+            <hr />
+            <p class="card-text">List of institutes</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-4 col-sm-12">
+        <div class="card ml-1 mr-1">
+          <div class="card-body">
+            <h4 class="card-title text-center">Useful Links</h4>
+            <hr />
+            <div class="row">
+              <div class="col-6">
+                <a href="/results/"
+                  ><button
+                    type="button"
+                    class="btn btn-lg btn-block btn-custom-css"
+                  >
+                    Results
+                  </button></a
+                >
+              </div>
+              <div class="col-6">
+                <a href="/downloads/"
+                  ><button
+                    type="button"
+                    class="btn btn-lg btn-block btn-custom-css"
+                  >
+                    Downloads
+                  </button></a
+                >
+              </div>
             </div>
-            <div class="card col-lg-4 col-md-4 col-sm-12">
-                <div class="card-body">
-                    <h4 class="card-title text-center">Institutes</h4>
-                    <hr />
-                    <p class="card-text">List of institutes</p>
-                </div>
+            <br />
+            <div class="row">
+              <div class="col-6">
+                <a href="/institute/"
+                  ><button
+                    type="button"
+                    class="btn btn-lg btn-block btn-custom-css"
+                  >
+                    Institutes
+                  </button></a
+                >
+              </div>
+              <div class="col-6">
+                <a href="/circulars/"
+                  ><button
+                    type="button"
+                    class="btn btn-lg btn-block btn-custom-css"
+                  >
+                    Circulars
+                  </button></a
+                >
+              </div>
             </div>
-            <div class="card col-lg-4 col-md-4 col-sm-12">
-                <div class="card-body">
-                    <h4 class="card-title text-center">Useful Links</h4>
-                    <hr />
-                    <div class="row">
-                        <div class="col-6">
-                            <a href="/results/"><button type="button" class="btn btn-lg btn-block btn-custom-css" >Results</button></a>
-                        </div>
-                        <div class="col-6">
-                            <a href="/downloads/"><button type="button" class="btn btn-lg btn-block btn-custom-css" >Downloads</button></a>
-                        </div>
-                    </div>
-                    <br />
-                    <div class="row">
-                        <div class="col-6">
-                            <a href="/institute/"><button  type="button" class="btn btn-lg btn-block btn-custom-css" >Institutes</button></a>
-                        </div>
-                        <div class="col-6">
-                            <a href="/circulars/"><button type="button" class="btn btn-lg btn-block btn-custom-css" >Circulars</button></a>
-                        </div>
-                    </div>
-                    <br />
-                    <div class="row">
-                        <a href="/online-admissions/" style="width:100%"><button type="button" class="btn btn-lg btn-block" style="background: #FF4E50;  /* fallback for old browsers */
+            <br />
+            <div class="row">
+              <a href="/online-admissions/" style="width:100%"
+                ><button
+                  type="button"
+                  class="btn btn-lg btn-block"
+                  style="background: #FF4E50;  /* fallback for old browsers */
                         background: -webkit-linear-gradient(to right, #F9D423, #FF4E50);  /* Chrome 10-25, Safari 5.1-6 */
                         background: linear-gradient(to right, #F9D423, #FF4E50); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-                        ">Online Admission</button></a>
-                    </div>
-                </div>
+                        "
+                >
+                  Online Admission
+                </button></a
+              >
             </div>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </div>

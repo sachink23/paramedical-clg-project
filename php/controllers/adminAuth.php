ebcbb8bda9c3b60d9934f3584c32e8082ec744fb
nodejs-machine <kekarjawalekarsachin1@gmail.com>
2019-07-31 19:35:07 +0530

    updated admin auth

--- php/controllers/adminAuth.php
+++ php/controllers/adminAuth.php
@@ -12,6 +12,8 @@
 			$array['last_name'] = trim($array['last_name']);
             $array['username'] = trim($array['username']);
             $array['email'] = trim($array['email']);
+            $array['created_by'] = trim($array['created_by']);
+             
 			if(!(preg_match('/^[a-zA-Z ]*$/', $array['first_name']) && preg_match('/^[a-zA-Z ]*$/', $array['last_name'])) && !(isset($array['first_name']) && isset($array['last_name']))) {
 				return array(false, 'Invalid Name');
             }
@@ -21,15 +23,20 @@
 			}
 			if(strlen($array['password']) < 8) {
 				return array(false, 'Short Password');
-			}
-			$array['password'] = $this->hashAdminPass($array['password']);
-            if (!filter_var($array['email'], FILTER_VALIDATE_EMAIL)) {
-                return array(false, 'invalid email format');
             }
+            $array['password'] = $this->hashAdminPass($array['password']);
+            if (!filter_var($array['email'], FILTER_VALIDATE_EMAIL)) {
+                return array(false, 'Invalid Email Format');
+            }
+            if(!(((is_bool($array['access_website_basic']) && is_bool($array['access_circulars'])) && (is_bool($array['access_admissions']) && is_bool($array['access_results']))) && is_bool($array['access_admin_creation']))) {
+                return array(false, 'Admin Access Is Not Properly Defined');
+            }
+            
+            
 
 			$db = new db;
 			
-			$format = 'ssss';
+			$format = 'ssssssiiiii';
 			$result = $db->insert($this->table, $array, $format);
 			if($result[0] == true) {
 				return true;
@@ -57,22 +64,27 @@
 						$_SESSION['admin_username'] = $row['username'];
 						$_SESSION['admin_id'] = $row['admin_id'];
 						$_SESSION['admin_f_name'] = $row['first_name'];
-						$_SESSION['admin_l_name'] = $row['last_name'];
+                        $_SESSION['admin_l_name'] = $row['last_name'];
+                        $_SESSION['admin_access_website_basic'] = $row['access_website_basic'];
+                        $_SESSION['admin_access_circulars'] = $row['access_circulars'];
+                        $_SESSION['admin_access_admissions'] = $row['access_admissions'];
+                        $_SESSION['admin_access_results'] = $row['access_results'];
+                        $_SESSION['admin_access_admin_creation'] = $row['access_admin_creation'];
 					}
 					else {
 						$this->logOutAdmin();
-						return array(false, 'invalid username or password');
+						return array(false, 'Invalid username or password');
 					}
 				}
 				$row = NULL;
 				$result = NULL;
-				return array(true, 'login success');
+				return array(true, 'Login Success');
 			}
 			else if($result[0] == true && $result[1]->num_rows == 0) {
- 				return array(false, 'invalid username or password');
+ 				return array(false, 'Invalid username or password');
 			}
 			else {
-				return array(false, 'something went wrong');
+				return array(false, 'Something went wrong, please try again');
 			}	
 		}
 
@@ -99,10 +111,16 @@
 
 		function logOutAdmin() {
 			$_SESSION['admin_username'] = NULL;
-			$_SESSION['admin_id'] = NULL;
-			$_SESSION['admin_f_name'] = NULL;
-			$_SESSION['admin_l_name'] = NULL;
-			session_unset();
+            $_SESSION['admin_id'] = NULL;
+            $_SESSION['admin_f_name'] = NULL;
+            $_SESSION['admin_l_name'] = NULL;
+            $_SESSION['admin_access_website_basic'] = NULL;
+            $_SESSION['admin_access_circulars'] = NULL;
+            $_SESSION['admin_access_admissions'] = NULL;
+            $_SESSION['admin_access_results'] = NULL;
+            $_SESSION['admin_access_admin_creation'] = NULL;
+            session_unset();
+            session_destroy();
 			return true;
 		}
 

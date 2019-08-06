55ff92b785f8b9248d63b04043a29c63990c9c81
nodejs-machine <kekarjawalekarsachin1@gmail.com>
2019-08-06 23:50:31 +0530

    updated 404

--- frontend/errors/404.php
+++ frontend/errors/404.php
@@ -1,1 +1,26 @@
-this is 404
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="<?=webCdn ?>/bootstrap/css/bootstrap.min.css">
+    <title>404 - Page Not Found : <?= appName; ?></title>
+    <style>
+        
+    </style>
+</head>
+<body style="background-image:url('https://images.pexels.com/photos/66874/landscape-meadow-field-mountains-66874.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=1366')">
+    <div class="container text-center my-5">
+        <div class="jumbotron">
+            <h1 class="display-3 text-danger">Error 404</h1>
+            <p class="lead text-info">Page you are looking for is not found, either it has been moved to another place or it doesnt exists..</p>
+            <hr class="my-2">
+            <p class=" text-info">If you think you have landed here by mistake, click the button below to return to home page.</p>
+            <p class="lead">
+                <a class="btn btn-primary btn-lg" href="/" role="button">Back to Homepage</a>
+            </p>
+        </div>
+    </div>
+</body>
+</html>

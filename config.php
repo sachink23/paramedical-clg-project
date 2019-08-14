96938e57c102cee8a4c168401efa47bb05c6df88
nodejs-machine <kekarjawalekarsachin1@gmail.com>
2019-08-14 18:36:41 +0530

    changed db credentials

--- config.php
+++ config.php
@@ -1,10 +1,4 @@
 <?php
-
-	/* 
-		enter domain name in appHost : line 18
-		enter database details on line 45
-
-	*/
 	# comment below three lines when you move to production
 	ini_set('display_errors', 1);
 	ini_set('display_startup_errors', 1);
@@ -42,7 +36,7 @@
 	define('fileUploadDir', appRoot.'/assets/uploads/');
 
 	#db related information
-	define('dbHost', 'localhost');
+	define('dbHost', 'dbs1.cwsbxjeoomse.ap-south-1.rds.amazonaws.com');
 	define('dbUser', 'sachin');
 	define('dbPass', 'Escpk4739J');
 	define('dbName', 'paraclg');

fe390d6e40c4b026bd668168db687478b8d1895a
nodejs-machine <kekarjawalekarsachin1@gmail.com>
2019-08-16 20:03:30 +0530

    stled courses page as per layout decided

--- frontend/website/pages/courses.php
+++ frontend/website/pages/courses.php
@@ -1,39 +1,40 @@
-<div class="container table-responsive">
-    <table id="coursesTable" class="table table-bordered table-inverse">
-        <thead class="thead-inverse">
-            <tr>
-                <th>Sr No</th>
-                <th>Course Name</th>
-                <th>Eligibility</th>
-                <th>Duration (Months)</th>
-                <th>Exam Fees (Rs)</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-                $db = new db;
-                $res = $db->select("courses", "*");
-                if($res[0] == true && $res[1]->num_rows > 0) {
-                    $i=0;
-                    while($row = mysqli_fetch_assoc($res[1])) {
-                        echo "<tr>"; 
-                        echo "<td scope='row'>".++$i."</td>";
-                        echo "<td>".$row['course_name']."</td>";
-                        echo "<td>".$row['eligibility']."</td>";
-                        echo "<td>".$row['duration']."</td>";
-                        echo "<td>".$row["exam_fees"]."</td>";
-                        echo "</tr>"; 
+<section id="courses">
+    <div class="container table-responsive">
+        <table id="coursesTable" class="table table-bordered table-inverse">
+            <thead class="thead-inverse">
+                <tr>
+                    <th>Sr No</th>
+                    <th>Course Name</th>
+                    <th>Eligibility</th>
+                    <th>Duration (Months)</th>
+                    <th>Exam Fees (Rs)</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    $db = new db;
+                    $res = $db->select("courses", "*");
+                    if($res[0] == true && $res[1]->num_rows > 0) {
+                        $i=0;
+                        while($row = mysqli_fetch_assoc($res[1])) {
+                            echo "<tr>"; 
+                            echo "<td scope='row'>".++$i."</td>";
+                            echo "<td>".$row['course_name']."</td>";
+                            echo "<td>".$row['eligibility']."</td>";
+                            echo "<td>".$row['duration']."</td>";
+                            echo "<td>".$row["exam_fees"]."</td>";
+                            echo "</tr>"; 
+                        }
+                    } else {
+                        echo "<tr><td colspan='5'>No Courses Found</td></tr>";
                     }
-                } else {
-                    echo "<tr><td colspan='5'>No Courses Found</td></tr>";
-                }
 
-            ?>            
-                
-        </tbody>
-    </table>
-</div>
-
+                ?>            
+                    
+            </tbody>
+        </table>
+    </div>
+</section>
 
 <script>
 $(document).ready(function() {

958c0d71d187574aa8db05617ef1cf2b14e73382
nodejs-machine <kekarjawalekarsachin1@gmail.com>
2019-08-24 20:25:13 +0530

    minor fix

--- frontend/website/pages/home.php
+++ frontend/website/pages/home.php
@@ -3,7 +3,7 @@
     <h2 class="text-uppercase text-center text-white">About</h2>
     <hr class="star-light mb-5" />
     <div class="row">
-      <div class="col-lg-4 ml-auto">
+      <div class="col-lg-8 mx-auto">
         <p class="lead">          
             <?= $bs['about_para_1']; ?>        <br />
             <?= $bs['about_para_2']; ?>        

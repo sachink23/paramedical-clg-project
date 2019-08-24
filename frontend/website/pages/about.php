db48b492642f2d595ac0f5e47808f83d3b32a74e
nodejs-machine <kekarjawalekarsachin1@gmail.com>
2019-08-24 20:31:48 +0530

    minor chages in about section

--- frontend/website/pages/about.php
+++ frontend/website/pages/about.php
@@ -5,7 +5,7 @@
         <div class="row">
             <div class="col-lg-8 mx-auto">
                 <p class="lead">          
-                    <?= $bs['about_para_1']; ?>        <br />
+                    <?= $bs['about_para_1']; ?>        <br /><br />
                     <?= $bs['about_para_2']; ?>        
                 </p>
             </div>
